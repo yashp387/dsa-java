fc7a9ea50d66cbe135f72949b263d38d3d5793ab
yashp387 <yashmp2004@gmail.com>
2025-07-03 09:38:52 +0530

    string sorting

---
+++ Strings/sortString.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class sortString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        // Convert string to char array
+        char[] chars = input.toCharArray();
+
+        // Sort the char array
+        for (int i = 0; i < chars.length - 1; i++) {
+            for (int j = i + 1; j < chars.length; j++) {
+                if (chars[i] > chars[j]) {
+                    // Swap
+                    char temp = chars[i];
+                    chars[i] = chars[j];
+                    chars[j] = temp;
+                }
+            }
+        }
+
+        // Convert back to string
+        String sorted = new String(chars);
+        System.out.println("Sorted string: " + sorted);
+        sc.close();
+    }
+}
+
+// time-complexity  -> O(n);
+// space complexity  -> O(1);
