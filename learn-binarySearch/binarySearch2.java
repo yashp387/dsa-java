705961c6cf5b29f88f5431eca2e6eb9dc0c2037e
yashp387 <yashmp2004@gmail.com>
2025-07-12 11:11:23 +0530

    learn lower bound in bs

--- learn-binarySearch/binarySearch2.java
+++ learn-binarySearch/binarySearch2.java
@@ -1,11 +1,11 @@
 
-// usign recursion
+// using recursion
 public class binarySearch2 {
     public static int binary_search(int[] arr, int low, int high, int target) {
         if(low > high) return -1;
 
         int mid = low + (high - low) / 2;
-        if(arr[mid] ==  target) return mid;
+        if(arr[mid] == target) return mid;
         else if(target > arr[mid]) {
             return binary_search(arr, mid + 1, high, target);
         }
---
+++ learn-binarySearch/lowerBound.java
@@ -0,0 +1,22 @@
+
+// brute forcee method
+public class lowerBound {
+    public static int lower_bound(int[] arr, int n, int x) {
+        for(int i = 0; i < n; i++) {
+            if(arr[i] >= x) {
+                return i;
+            }
+        }
+        return n;
+    }
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 8, 15, 19};
+        int n = arr.length;
+        int x = 9;
+        int indx = lower_bound(arr, n, x);
+        System.out.println("The lower bound is at index: "+ indx);
+    }
+}
+
+// time complexity  -> O(n);
+// space complexity -> O(1);
